1547736b925a66beedbbbce9dab441db5075338e
Titewithane <156405660+Passakorn-Aiamwasu@users.noreply.github.com>
2024-02-23 14:55:16 +0700

    Finish?

--- main.c
+++ main.c
@@ -7,23 +7,42 @@
 
 int main(int argc , char **argv) {
   NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
-/* For struct Queue
+  NodePtr tailPtr=NULL;
+  int y;
+  /*enqueue(&headPtr,&tailPtr,5);
+  enqueue(&headPtr,&tailPtr,6);
+  enqueue(&headPtr,&tailPtr,7);
+  enqueue(&headPtr,&tailPtr,8);
+  enqueue(&headPtr,&tailPtr,9);
+  y=dequeue(&headPtr,&tailPtr);
+  y=dequeue(&headPtr,&tailPtr);
+  printf("%d\n",y);*/
+
+
+
+
+
+//For struct Queue
   Queue  q;
    q. headPtr=NULL;
    q.tailPtr=NULL;
    q.size=0;
-*/
+
    int i,x;
-   
+
+   /*enqueue_struct(&q,7);
+   printf("%d\n",dequeue_struct(&q));
+   printf("%d\n",dequeue_struct(&q));
+   enqueue_struct(&q,7);
+   printf("%d\n",dequeue_struct(&q));*/
 
  for(i=1;i<argc;i++){
         if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
+            x=dequeue_struct(&q);
             printf("dequeing %d\n",x);
         }
         else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
+       enqueue_struct(&q, atoi(argv[i]));
            
         }
  }
